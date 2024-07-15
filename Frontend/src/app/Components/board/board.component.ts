9d461bc7f3be5f70036f71e42e160e3ccb2de51d
Mafe Gamboa <158193517+Mafegam@users.noreply.github.com>
2024-07-15 16:53:21 -0500

    Fixed drang and drop

--- Frontend/src/app/Components/board/board.component.ts
+++ Frontend/src/app/Components/board/board.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [NavBarComponent, FooterComponent],
+  imports: [NavBarComponent, FooterComponent, CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
 export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  
+  progress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
+
