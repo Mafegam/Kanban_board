b8d9da45599c3d82603498670f708821a3baa44e
Mafe Gamboa <158193517+Mafegam@users.noreply.github.com>
2024-07-16 11:09:29 -0500

    Created input and tasks array, drag and drop works

--- Frontend/src/app/Components/board/board.component.ts
+++ Frontend/src/app/Components/board/board.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { newTask } from '../../Interfaces/task'
 
 import {
   CdkDragDrop,
@@ -9,7 +10,7 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
-
+import { NgFor } from '@angular/common';
 
 import { ChangeDetectionStrategy, inject, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -35,20 +36,23 @@ export interface DialogData {
   selector: 'app-board',
   standalone: true,
   imports: [NavBarComponent, FooterComponent, CdkDropList, CdkDrag,
-    MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule
+    MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, NgFor
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
 
-
 export class BoardComponent {
-  ss = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  progress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  addedTask: any;
+  tasksArray: any = [];
+  progress: any = [];
+  done: any = [];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  add(){
+    this.tasksArray.push(this.addedTask)
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -64,47 +68,52 @@ export class BoardComponent {
   }
 
   // BUTTON
-  readonly todo = signal('');
-  readonly name = model('');
-  readonly dialog = inject(MatDialog);
+//   readonly todo = signal('');
+//   readonly name = model('');
+//   readonly dialog = inject(MatDialog);
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: { name: this.name(), todo: this.todo() },
-    });
+//   openDialog(): void {
+//     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+//       data: { name: this.name(), todo: this.todo() },
+//     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      if (result !== undefined) {
-        this.todo.set(result);
-      }
-    });
-  }
-}
+//     dialogRef.afterClosed().subscribe(result => {
+//       console.log('The dialog was closed');
+//       if (result !== undefined) {
+//         this.todo.set(result);
+//       }
+//     });
+//   }
+
+
+// }
 
 // POP UP
-@Component({
-  selector: 'dialog',
-  templateUrl: './dialog.component.html',
-  standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatButtonModule,
-    MatDialogTitle,
-    MatDialogContent,
-    MatDialogActions,
-    MatDialogClose,
-  ],
-})
-export class DialogOverviewExampleDialog {
-  readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
-  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
-  readonly todo = model(this.data.todo);
+// @Component({
+//   selector: 'dialog',
+//   templateUrl: './dialog.component.html',
+//   standalone: true,
+//   imports: [
+//     MatFormFieldModule,
+//     MatInputModule,
+//     FormsModule,
+//     MatButtonModule,
+//     MatDialogTitle,
+//     MatDialogContent,
+//     MatDialogActions,
+//     MatDialogClose,
+//   ],
+// })
+// export class DialogOverviewExampleDialog {
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+//   readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
+//   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+//   readonly todo = model(this.data.todo);
+
+//   onNoClick(): void {
+//     this.dialogRef.close();
+//   }
+
+  
 }
 
